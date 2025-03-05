979f3df2f981c9481ef631b741b9f83417de542c
Kiselev Oleg <kiselevoleg2016@gmail.com>
2025-03-05 23:16:34 +0300

    basis images for matrix pencil method resultivity has been added with needed for adding more dx changing filter for check filtering false points

--- graphics/matrix_pencil_method/script.m
+++ graphics/matrix_pencil_method/script.m
@@ -5,3 +5,13 @@ plot(A(:,1),A(:,2),'.b');
 hold('on');
 plot(A(:,1),A(:,3),'.r');
 
+A=load('../distersion_curve_for_K/dispersion_curves.data');
+plot(A(:,1),A(:,2),'.k');
+
+xlim([0 2]);
+ylim([0 140/4]);
+%xlim([0 1.5]);
+%ylim([0 35]);
+set(gca,'FontSize',24,'fontWeight','bold');
+set(findall(gcf,'type','text'),'FontSize',24,'fontWeight','bold');
+
